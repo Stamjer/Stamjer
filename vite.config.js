c9ad1bd03228e11494c7f4b75dd8e3088ccd04e2
rskort <r.s.kort@umail.leidenuniv.nl>
2025-07-10 22:08:12 +0200

    Moved index.html and added /docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react           from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Stamjer/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -26,5 +27,8 @@ export default defineConfig({
     'process.env': {
       VITE_API_BASE_URL: JSON.stringify('/api')
     }
+  },
+  build: {
+    outDir: 'docs'
   }
 })
